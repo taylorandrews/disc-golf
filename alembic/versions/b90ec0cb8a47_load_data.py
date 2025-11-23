fd4e213c7036253d466e8e9f14e0e1ba04dbaeb9
Taylor Andrews <twandrews@icloud.com>
2025-11-23 09:54:54 -0700

    WIP: bus updates for round data

--- alembic/versions/b90ec0cb8a47_load_data.py
+++ alembic/versions/b90ec0cb8a47_load_data.py
@@ -25,7 +25,7 @@ depends_on: Union[str, Sequence[str], None] = None
 
 path_to_pdga_data = "data/pdga/"
 path_to_seeds = "data/seed/"
-
+verbose = True
 
 def upgrade() -> None:
     """Loading Data."""
@@ -71,6 +71,21 @@ def upgrade() -> None:
     def to_bool(value: str) -> bool:
         return value.lower() in ("yes", "true", "t", "1")
     
+    def get_round_info_from_file_name(file_name, tournaments):
+        file_name_parts = file_name.split("_")
+        tournament_id = file_name_parts[1]
+        for tournament in tournaments:
+            if tournament["tournament_id"] == tournament_id:
+                tournament_name = tournament["name"]
+                tournament_start_date = tournament["start_date"]
+        round_num = file_name_parts[-1].replace(".json", "")
+        round_info = {
+            "tournament_name": tournament_name,
+            "round_num": round_num,
+            "tournament_start_date": tournament_start_date
+        }
+        return round_info
+    
     def get_tournaments(path_to_tournament_seed):
         with open(path_to_tournament_seed, mode='r', newline='') as seed_file:
             dictreader = csv.DictReader(seed_file)
@@ -89,8 +104,9 @@ def upgrade() -> None:
                 )
             return tournaments
     
-    def get_holes(data):
+    def get_holes_and_rounds(data, round_start_date):
         holes = []
+        rounds = []
         for pool in data:
             round_id = pool["live_round_id"]
             if len(pool["layouts"]) > 1:
@@ -99,6 +115,7 @@ def upgrade() -> None:
             for layout in pool["layouts"]:
                 course_id = layout["CourseID"]
                 layout_id = layout["LayoutID"]
+                tournament_id = layout["TournID"]
                 hole_detail = layout["Detail"]
                 length_units = layout["Units"]
                 hole_reference = {}
@@ -108,7 +125,7 @@ def upgrade() -> None:
                     # Get at this like: hole_reference["H1"]["Length"]
                 for person_round in pool["scores"]:
                     if person_round["HasRoundScore"] == 1:
-                        person_round_id = person_round["ResultID"]
+                        person_round_id = person_round["ScoreID"]
                         pdga_number = person_round["PDGANum"]
                         tournament_round_num = person_round["Round"]
                         won_playoff = person_round["WonPlayoff"]
@@ -117,32 +134,41 @@ def upgrade() -> None:
                         hole_count = person_round["Holes"]
                         card_number = person_round["CardNum"]
                         tee_time = person_round["TeeTime"]
-                        round_rating = person_round["RoundRating"]
+                        round_to_par = int(person_round["RoundtoPar"])
+                        round_rating = int(person_round["RoundRating"])
+                        rounds.append(
+                            {
+                                "round_id": person_round_id,
+                                "layout_id": layout_id,
+                                "course_id": course_id,
+                                "pdga_number": pdga_number,
+                                "tournament_id": tournament_id,
+                                "tournament_round_id": round_id,
+                                "tournament_round_num": tournament_round_num,
+                                "won_playoff": won_playoff,
+                                "prize": prize,
+                                "round_status": round_status,
+                                "hole_count": hole_count,
+                                "card_number": card_number,
+                                "tee_time": tee_time,
+                                "round_rating": round_rating,
+                                "round_score": round_to_par,
+                                "round_score": round_date
+                            }
+                        )
                         for i, hole_score in enumerate(person_round["HoleScores"]):
                             hole_ref = hole_reference[f"H{i+1}"]
                             holes.append(
                                 {
-                                    "hole_id": str(layout_id) + "_H" + str(i+1),
-                                    "layout_id": layout_id,
-                                    "course_id": course_id,
-                                    "round_id": round_id,
-                                    "person_round_id": person_round_id,
+                                    "hole_id": str(person_round_id) + "_H" + str(i+1),
+                                    "round_id": person_round_id,
                                     "pdga_number": pdga_number,
-                                    "tournament_round_num": tournament_round_num,
-                                    "won_playoff": won_playoff,
-                                    "prize": prize,
-                                    "round_status": round_status,
-                                    "hole_count": hole_count,
-                                    "card_number": card_number,
-                                    "tee_time": tee_time,
-                                    "round_rating": round_rating,
                                     "hole_number": hole_ref["HoleOrdinal"],
                                     "par": hole_ref["Par"],
                                     "length": hole_ref["Length"] if length_units == "Feet" else hole_ref["Length"]*3.280833,
                                     "score": hole_score,
                                 }
                             )
-        print(f"Loaded {len(holes)} holes")
         return holes
 
     def run_upserts(
@@ -179,21 +205,31 @@ def upgrade() -> None:
         op.execute(courses_upsert)
         op.execute(players_upsert)
         op.execute(holes_upsert)
-        
 
     tournaments = get_tournaments(path_to_seeds + "tournament_data.csv")
+    print("Found", len(tournaments) , "tournaments in the seed file")
 
     for file_name in os.listdir(path_to_pdga_data):
         # read 1 round of data
         round_data = read_round("/".join([path_to_pdga_data, file_name]))
-
-        # make list of dicts for each table
         preprocessed_round_data = round_data["data"]
         if type(preprocessed_round_data) is not list:
             preprocessed_round_data = [preprocessed_round_data]
+
+        round_info = get_round_info_from_file_name(file_name, tournaments)
+        if verbose:
+            print("Processing", round_info["tournament_name"], "round", round_info["round_num"])
+        
+        # make list of dicts for each table
         courses = get_courses(preprocessed_round_data)
+        if verbose:
+            print("Found", len(courses) , "course(s)")
         players = get_players(preprocessed_round_data)
-        holes = get_holes(preprocessed_round_data)
+        if verbose:
+            print("Found", len(players) , "players")
+        holes = get_holes_and_rounds(preprocessed_round_data, round_info["stare_date"])
+        if verbose:
+            print("Found", len(holes) , "holes played")
 
         # alembic bulk upserts
         run_upserts(
